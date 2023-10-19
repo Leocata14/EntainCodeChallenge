684ae67e5dbf89525ddd945dcb21315109fd0d67
Jason Leocata <jl@jasonleocata.com.au>
2023-10-20 07:47:17 +1100

    Initial Commit

---
+++ EntainCodeChallenge/EntainCodeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntainCodeChallengeApp.swift
+//  EntainCodeChallenge
+//
+//  Created by Jason Leocata on 20/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct EntainCodeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
